c1b8c15a65973f41e77e0f2859c2456c2bd9077d
mmews <marcus.mews@numberfour.eu>
2018-08-23 10:55:32 +0200

    setup for publishing job

--- publish.sh
+++ publish.sh
@@ -6,48 +6,117 @@ set -e +x
 
 #export NPM_CONFIG_GLOBALCONFIG=/Users/marcus.mews/GitHub/n4jsd/
 echo "== Start publishing"
+DIR=`dirname $0`
 
-echo "   Run yarn install"
-yarn install
+# The first parameter is the url to npm registry (http://localhost:4873), if not exists then exit
+if [ -z "$1" ]; then
+	export NPM_REGISTRY="http://webclients1-nexus.service.cd-dev.consul/repository/npm-internal"
+	echo "The url to npm registry must be specified."
+	echo "Using default registry: $NPM_REGISTRY"
+	#exit -1
+else
+	export NPM_REGISTRY=$1
+fi
 
-echo "   export PATH"
+
+echo "Run yarn install"
+export NPM_CONFIG_GLOBALCONFIG="${DIR}"
+npm install --registry=http://nexus3-aws.corp.numberfour.eu/repository/npm-public/
+echo "export PATH"
 export PATH=`pwd`/node_modules/.bin:${PATH}
 
-# Set N4_N4JSC_JAR to the the freshly built n4jsc.jar in tools/hlc/target
-#export N4_N4JSC_JAR="${CUR_DIR}/tools/org.eclipse.n4js.hlc/target/n4jsc.jar"
 
-echo "   starting verdaccio"
-verdaccio &
+
+
+echo "check for old verdaccio"
+set +e # ignore problems
+OLD_VERDACCIO_PID="$(lsof -ti :4873)"
+if [ $? -eq 0 ]; then
+	echo "kill old verdaccio"
+	kill $OLD_VERDACCIO_PID
+fi
+set -e
+
+
+
+echo "clean artifacts of old verdaccio"
+rm -rf ./storage
+
+
+
+echo "starting new verdaccio"
+verdaccio --config config.yaml &
 VERDACCIO_PID=$!
 echo "   sleep 1s"
 sleep 1s
 
-PACKAGE_JSON="package.json"
-PACKAGE_JSON_LENGTH=${#PACKAGE_JSON}
 
 
-pushd npm
-	echo "   run in every"
-	for PCKJSON in $(find . -name $PACKAGE_JSON);
-	do
-		LENGTH=${#PCKJSON}
-		LOCATION=${PCKJSON:0:LENGTH-PACKAGE_JSON_LENGTH}
-		pushd $LOCATION
-			#echo "publish $LOCATION";
-			set +e # ignore problems
-			#npm publish --access=public --registry=http://localhost:4873
-			subl .project
-			set -e
-		popd
-	done
-popd
-
-#lerna exec npm publish --access=public --registry=http://localhost:4873
+echo "publish to local verdaccio"
+# works always since verdaccio is clean and fresh
+lerna exec "npm publish --registry=http://localhost:4873;"
 
 
-echo "   killing verdaccio"
+
+
+echo "get all projects"
+PRJ_LOCS="$(lerna exec pwd)"
+PRJ_COUNT=0
+for PRJ_LOC in $PRJ_LOCS;
+do
+	PRJ_COUNT=$[$PRJ_COUNT +1];
+done
+echo "found $PRJ_COUNT projects"
+
+
+
+
+echo "validate all n4jsd projects separately"
+COUNT=1
+ERRORS=""
+N4JSC_NPMRC=${DIR}/n4jsc_npmrc
+export NPM_CONFIG_GLOBALCONFIG="${N4JSC_NPMRC}" # this may be obsolete due to argument '--npmrcRootLocation'
+echo "using .npmrc file from $N4JSC_NPMRC"
+set +e # skip problems
+for PRJ_LOC in $PRJ_LOCS;
+do
+	echo "validate $COUNT of $PRJ_COUNT: $PRJ_LOC"
+	OUTPUT="$(n4jsc --npmrcRootLocation $N4JSC_NPMRC -imd -bt projects $PRJ_LOC 2>&1)"
+
+	if [[ $OUTPUT = *"ERROR:"* ]]; then
+		echo "There were errors in the output:"
+		echo "$OUTPUT"
+		ERRORS="$OUTPUT"
+	fi
+
+	COUNT=$[$COUNT +1];
+done
+export NPM_CONFIG_GLOBALCONFIG="${DIR}" # reset global variable
+
+
+echo "killing verdaccio"
 set +e # ignore problems
 kill $VERDACCIO_PID
 
 
+
+
+if [ ${#ERRORS} -ne 0 ]; then
+	echo "There were errors during validating all n4jsd projects. See output above."
+	echo "exit."
+	exit -1;
+fi
+
+
+
+
+
+
+echo "publish to $NPM_REGISTRY"
+lerna exec "set +e; npm publish --access=public --registry=$NPM_REGISTRY; set -e;"
+
+
+
 echo "== Publishing done."
+
+
