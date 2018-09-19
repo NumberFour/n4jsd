a72897661dbd11e91e8b516ffcb161c3e9549203
mmews <marcus.mews@numberfour.eu>
2018-09-19 16:58:14 +0200

    change retrieval of name and version to use node

--- publishSingleNPM.sh
+++ publishSingleNPM.sh
@@ -11,18 +11,14 @@ if [[ $# -eq 0 ]]; then
 fi
 
 REGISTRY=$1
-VERSION="$( npm view --registry="$REGISTRY" . version )"
-NAME="$( npm view --registry="$REGISTRY" . name )"
+NAME=$(node -p -e "require('./package.json').name")
+VERSION=$(node -p -e "require('./package.json').version")
 RESULT=" $(npm view --registry="$REGISTRY" "${NAME}@${VERSION}" )"
+
 if [[ -z "${RESULT// }" ]]; then
 	echo "publishing ${NAME}@${VERSION} to registry $REGISTRY"
 	npm publish --access=public --registry="$REGISTRY"
 else
 	echo "skipping already published package ${NAME}@${VERSION}"
-	echo "result was"
-	echo "$RESULT"
 fi
 
-cat package.json
-echo "_________________________________"
-echo "---------------------------------"
