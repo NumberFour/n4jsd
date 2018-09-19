832c14ac5b196161265e5621b934bf8e209ec48c
mmews <marcus.mews@numberfour.eu>
2018-09-19 17:05:38 +0200

    set to public npmjs.org and activate validations

--- publish.sh
+++ publish.sh
@@ -112,7 +112,7 @@ echo "using .npmrc file from $NPMRC_VERDACCIO"
 for PRJ_LOC in $PRJ_LOCS;
 do
 	echo "validate $COUNT of $PRJ_COUNT: $PRJ_LOC"
-	#OUTPUT="$(n4jsc --npmrcRootLocation $NPMRC_VERDACCIO -imd -bt projects $PRJ_LOC 2>&1)"
+	OUTPUT="$(n4jsc --npmrcRootLocation $NPMRC_VERDACCIO -imd -bt projects $PRJ_LOC 2>&1)"
 
 	if [[ $OUTPUT = *"ERROR:"* ]]; then
 		echo "There were errors in the output:"
